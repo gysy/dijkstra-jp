713e0e33bce5814a415ddfaebdb3275d0acf1df3
syita <guoyuan000@gmail.com>
2014-04-11 09:36:08 +0800

    small conflict handle

--- src/signup/models.py
+++ src/signup/models.py
@@ -8,28 +8,17 @@ from jpclass.models import Ngrade
 
 
 class Examdate(models.Model):
-<<<<<<< HEAD
     date = models.CharField(unique=True, max_length=10)
     def __unicode__(self):
         return self.date
 
-=======
-    year = models.IntegerField()
-    month = models.IntegerField(unique=True)
-    def __unicode__(self):
-        return smart_text('%i年%i月' % (self.year, self.month))
->>>>>>> 846b1a0d305c601b331e47061711359412ec2f64
 
 
 class Examsignup(models.Model):
     ngrade = models.ForeignKey(Ngrade)
     examdate = models.ForeignKey(Examdate)
     def __unicode__(self):
-<<<<<<< HEAD
         return smart_text('%s %s' % (self.examdate, self.ngrade))
-=======
-        return smart_text('%s 报名了 %s 的 日本语%s的考试' % (self.user, self.examdate, self.ngrade))
->>>>>>> 846b1a0d305c601b331e47061711359412ec2f64
 
 class ExamsignupAdmin(admin.ModelAdmin):
     list_display = ('id', 'ngrade', 'examdate')
